df1d4e2849daf3030257980dcc85722a727f1398
edouardmytton <79758744+edouardmytton@users.noreply.github.com>
2025-05-03 12:28:09 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'HansTz&BD8F3DjC#HF5dH_jgEVO-ZU9LQBrkGdaXUUP2XlKYXYiTCl2RecI',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
